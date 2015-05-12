738def2cd67316dca3f56b2e83ad5c7e54d583ef
OpenShift System User <554a7306fcf9339c09000123@ex-std-node480.prod.rhcloud.com>
2015-05-11 23:00:39 -0400

    remove thumbnails do content grid

--- framework/layouts/content-grid_2_column.php
+++ framework/layouts/content-grid_2_column.php
@@ -6,14 +6,6 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('col-md-6 col-sm-6 grid grid_2_column'); ?>>
 
-		<div class="featured-thumb col-md-12">
-			<?php if (has_post_thumbnail()) : ?>	
-				<a href="<?php the_permalink() ?>" title="<?php the_title() ?>"><?php the_post_thumbnail('pop-thumb'); ?></a>
-			<?php else: ?>
-				<a href="<?php the_permalink() ?>" title="<?php the_title() ?>"><img src="<?php echo get_template_directory_uri()."/assets/images/placeholder2.jpg"; ?>"></a>
-			<?php endif; ?>
-		</div><!--.featured-thumb-->
-			
 		<div class="out-thumb col-md-12">
 			<header class="entry-header">
 				<h1 class="entry-title title-font"><a class="hvr-underline-reveal" href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
